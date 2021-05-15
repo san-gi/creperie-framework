4090f5e833fd92ddda5aa91ec2c3037a2db02e72
Damien Deturck <deturckdamien@gmail.com>
2021-05-15 20:20:27 +0200

    save

---
+++ creperie/assets/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <test></test>
+</template>
+
+<script>
+import test from './Test.vue'
+
+export default {
+  components: {
+    test,
+  },
+}
+</script>
---
+++ creperie/assets/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>{{ test }}</h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: "Hello worldaaa"
+    };
+  }
+};
+</script>
